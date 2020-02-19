397c28ac007efda7192c45f1d5e0997d256d9085
craigmc08 <craigmc08@gmail.com>
2020-02-18 19:58:09 -0500

    Adds loading of obj file to Main rendering file

--- app/Main.hs
+++ app/Main.hs
@@ -2,74 +2,47 @@ module Main where
 
 import Lib
 import Types
-import Object.Vertex (createVertex)
 import Numeric.Vector
+import Wavefront.Wavefront
 
 cam :: Camera
-cam = Camera { c_getPos = vec3 0 0.2 (-5)
+cam = Camera { c_getPos = vec3 3 2 (-5)
              , c_getForward = vec3 0 0 (1)
              , c_getUp = vec3 0 1 0
              , c_getRight = vec3 1 0 0
              , c_getFOV = 90
              }
 
+checkerShader :: BRDF
+checkerShader = let spacing = VecConst 1 1 1
+                    quarterSpacing = VecMath VDiv spacing (VecConst 4 4 4)
+                    cs = VecMath
+                           VSub
+                           (VecConst 1 1 1)
+                           (VecMath
+                             VMul
+                             (VecConst 2 2 2) 
+                             (VecMath
+                               VDiv
+                               (VecMath
+                                 VMod
+                                 (VecMath
+                                   VAdd
+                                   quarterSpacing
+                                   (VecMath VAbs (VecMath VAdd UV quarterSpacing) (VecConst 0 0 0))
+                                 )
+                                 spacing
+                               )
+                               spacing
+                             )
+                           )
+                    lt = ValMath LessThan (ValMath Mul (SeparateX cs) (SeparateY cs)) (ValConst 0)
+                    value = ValMath Add (ValConst 0.7) (ValMath Mul (ValConst 0.3) lt)
+                    color = CombineRGB value value value
+                in Diffuse color
+
 shader :: BRDF
-shader = let spacing = VecConst 1 1 1
-             quarterSpacing = VecMath VDiv spacing (VecConst 4 4 4)
-             cs = VecMath
-                    VSub
-                    (VecConst 1 1 1)
-                    (VecMath
-                      VMul
-                      (VecConst 2 2 2) 
-                      (VecMath
-                        VDiv
-                        (VecMath
-                          VMod
-                          (VecMath
-                            VAdd
-                            quarterSpacing
-                            (VecMath VAbs (VecMath VAdd UV quarterSpacing) (VecConst 0 0 0))
-                          )
-                          spacing
-                        )
-                        spacing
-                      )
-                    )
-             lt = ValMath LessThan (ValMath Mul (SeparateX cs) (SeparateY cs)) (ValConst 0)
-             value = ValMath Add (ValConst 0.7) (ValMath Mul (ValConst 0.3) lt)
-             color = CombineRGB value value value
-         in Diffuse color
--- shader = Diffuse $ ColVector $ VecMath VAdd UV $ VecConst 0 0 1
-
-fstTri :: Tri
-fstTri = Tri { t_getP0 = createVertex (vec3 (-1) (-1) 0) (vec3 1 1 1) (vec2 0 0) (vec3 0 0 (-1)) (vec3 1 0 0)
-             , t_getP1 = createVertex (vec3 (-1) 1 0) (vec3 1 1 1) (vec2 0 1) (vec3 0 0 (-1)) (vec3 1 0 0)
-             , t_getP2 = createVertex (vec3 1 1 0) (vec3 1 1 1) (vec2 1 1) (vec3 0 0 (-1)) (vec3 1 0 0)
-             }
-
-sndTri :: Tri
-sndTri = Tri { t_getP0 = createVertex (vec3 1 1 0) (vec3 1 1 1) (vec2 1 1) (vec3 0 0 (-1)) (vec3 1 0 0)
-             , t_getP1 = createVertex (vec3 1 (-1) 0) (vec3 1 1 1) (vec2 1 0) (vec3 0 0 (-1)) (vec3 1 0 0)
-             , t_getP2 = createVertex (vec3 (-1) (-1) 0) (vec3 1 1 1) (vec2 0 0) (vec3 0 0 (-1)) (vec3 1 0 0)
-             }
-
-thdTri :: Tri
-thdTri = Tri { t_getP0 = createVertex (vec3 (-1) (-1) 0) (vec3 1 1 1) (vec2 0 0) (vec3 0 1 0) (vec3 1 0 0)
-             , t_getP1 = createVertex (vec3 1 (-1) (-1)) (vec3 1 1 1) (vec2 1 1) (vec3 0 1 0) (vec3 1 0 0)
-             , t_getP2 = createVertex (vec3 (-1) (-1) (-1)) (vec3 1 1 1) (vec2 0 1) (vec3 0 1 0) (vec3 1 0 0)
-             }
-
-fourTri :: Tri
-fourTri = Tri { t_getP0 = createVertex (vec3 (-1) (-1) 0) (vec3 1 1 1) (vec2 0 0) (vec3 0 1 0) (vec3 1 0 0)
-              , t_getP1 = createVertex (vec3 1 (-1) 0) (vec3 1 1 1) (vec2 1 0) (vec3 0 1 0) (vec3 1 0 0)
-              , t_getP2 = createVertex (vec3 1 (-1) (-1)) (vec3 1 1 1) (vec2  1 1) (vec3 0 1 0) (vec3 1 0 0)
-              }
-
-quad :: Object
-quad = Object { o_getTris = [fstTri, sndTri, thdTri, fourTri]
-              , o_getShader = shader
-              }
+shader = Diffuse $ ColConst 0.8 0.8 0.8
 
 sun :: Light
 sun = Directional { l_getAngle = 2.5
@@ -80,16 +53,22 @@ sun = Directional { l_getAngle = 2.5
 
 scene :: Scene
 scene = Scene { s_getCamera = cam
-              , s_getObjects = [quad]
+              , s_getObjects = []
               , s_getLights = [sun]
               , s_getSkyColor = vec3 0.6 0.7 0.9
               , s_getWidth = 640
               , s_getHeight = 480
               , s_getSeed = 1
-              , s_getSamples = 1024
-              , s_getBounces = 0
+              , s_getSamples = 128
+              , s_getBounces = 2
               }
 
 main :: IO ()
 main = do
-  writeImg "testimg.png" $ parRender scene
+  maybeCube <- readObj "cube.obj" <$> readFile "/home/craig/haskell/raytracer/app/cube.obj"
+  case maybeCube of
+    Left e -> print e
+    Right cube -> do
+      -- print cube
+      let scene' = scene { s_getObjects = [Object cube shader] }
+      writeImg "testimg.png" $ parRender scene'
